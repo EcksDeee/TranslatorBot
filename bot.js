fbb4e1f12cc3542f842d40c84a839415dd730f6f
EcksDeee <36055694+EcksDeee@users.noreply.github.com>
2018-05-17 14:34:27 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+const Music = require('discord.js-musicbot-addon');
+
+const client = new Discord.Client();
+
+var botstatus = true;
+
+require('events').EventEmitter.defaultMaxListeners = Infinity;
+
+const music = new Music(client, {
+    youtubeKey: process.env.ytapikey, //Youtube Dev API3 Key
+    prefix: config.prefix, maxQueueSize: 100, thumbnailType: 'default', defVolume: 100, anyoneCanSkip: true, messageHelp: true,
+    botOwner: '342681780635172864', helpCmd: 'assist', playCmd: 'play',    skipCmd: 'skip', queueCmd: 'queue',
+    pauseCmd: 'pause', resumeCmd: 'resume', volumeCmd: 'vol', leaveCmd: 'leave', clearCmd: 'clear', setCmd: 'set',
+    loopCmd: 'loop', searchCmd: 'search', ownerCmd: 'owner', enableQueueStat: true});
+
+client.on('ready', () => { 
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(config.defaultActivity);
+});
+
+client.on('message', msg => {
+    const pref = msg.content.slice(0, 2);
+    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if (pref === config.prefix) {
+                
+                
+                } else {
+                        return;
+                }
+        } else {
+                return;
+        }
+});
